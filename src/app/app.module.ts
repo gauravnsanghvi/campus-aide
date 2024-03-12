133b2ec4b19cf15c39e6bf5114abd8bbc67b1efb
gauravnsanghvi <gauravsanghvi80@gmail.com>
2024-03-12 07:26:26 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouteReuseStrategy } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { NotificationModalComponent } from './notification-modal/notification-modal.component';
@@ -12,7 +13,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, NotificationModalComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,ReactiveFormsModule,FormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { NotificationsService } from '../notifications.service';
+import { Component, OnInit, ViewChild} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ModalController } from '@ionic/angular'; // Import ModalController
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -8,6 +10,70 @@ import { NotificationsService } from '../notifications.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor() {}
+  token:string="";
+  ionicForm: FormGroup;
+  isSubmitClicked: boolean = false;
+  isEmailInvalid: boolean = false;
+  ispasswordInvalid:boolean = false;
+  @ViewChild('modal') modal: any;
+  message: string | undefined;
+  language: string = 'language';
+  isModalOpen = false;
+  constructor(private formBuilder: FormBuilder,private router: Router) {
+    this.ionicForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$])[a-zA-Z0-9!@#$]+$')
+      ])] 
+    });
+  
+    this.ionicForm.controls['email'].valueChanges.subscribe(() => {
+      this.isEmailInvalid = false;
+    });
+  
+    this.ionicForm.controls['password'].valueChanges.subscribe(() => {
+      this.ispasswordInvalid = false;
+    });
+  }
+  onSubmit(formData: any) {
+    this.isSubmitClicked = true; 
+    const emailControl = this.ionicForm.controls['email'];
+    const passwordControl = this.ionicForm.controls['password'];
 
+    if (emailControl.errors) {
+      if (emailControl.errors['required']) {
+        this.isEmailInvalid = true;
+        console.log('Email is required');
+        this.setOpen(false);
+      } else if (emailControl.errors['email']) {
+        this.isEmailInvalid = true;
+        this.setOpen(false);
+      }
+    } else {
+      console.log('Form submitted successfully');
+      console.log(formData);
+    }
+
+    if (passwordControl.errors) {
+      if (passwordControl.errors['required']) {
+        this.ispasswordInvalid = true;
+        console.log('Password is required');
+        this.setOpen(false);
+      } else if (passwordControl.errors['pattern']) {
+        this.ispasswordInvalid = true;
+        console.log('Password is invalid');
+        this.setOpen(false);
+      }
+    } else {
+      console.log('Form submitted successfully');
+      console.log(formData);
+      
+    }
+    
+  }
+  setOpen(isOpen: boolean) {
+    console.log(isOpen);
+    this.isModalOpen = isOpen;
+  }
 }
